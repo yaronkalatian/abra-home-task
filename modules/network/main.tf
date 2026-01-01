5e6a4296e03070f46ba3df1197d3d159f8b4d357
yaron kalatian <kalatianyaron@gmail.com>
2026-01-01 18:40:45 +0200

    add route table

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -44,3 +44,20 @@ resource "aws_nat_gateway" "abra" {
     Name = "${var.app_name}-nat-${count.index}"
   }
 }
+
+resource "aws_route_table" "private" {
+  vpc_id = var.vpc_id
+  tags   = { Name = "private-rt" }
+}
+
+resource "aws_route" "private_internet" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this[0].id   # adjust for multi-AZ if needed
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = var.private_subnets[count.index]
+  route_table_id = aws_route_table.private.id
+}
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -19,3 +19,13 @@ variable "app_name" {
   description = "Application name used for resource naming"
   type        = string
 }
+
+variable "vpc_id" {
+  description = "VPC ID where the ALB will be deployed"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "Private subnets for the internal ALB"
+  type        = list(string)
+}
