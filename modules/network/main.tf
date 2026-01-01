9dfdf9eebd02e23115ac47bb812db520b6eaf67f
yaron kalatian <kalatianyaron@gmail.com>
2026-01-01 18:50:38 +0200

    id for sn

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -58,6 +58,6 @@ resource "aws_route" "private_internet" {
 
 resource "aws_route_table_association" "private" {
   count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index]
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
