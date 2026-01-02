0a4b93dc835222745f81f9785048ab838504c5d1
yaron kalatian <kalatianyaron@gmail.com>
2026-01-02 11:52:31 +0200

    remove

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "api_gateway_endpoint" {
   description = "Public API Gateway endpoint"
   value       = module.api_gateway.api_endpoint
 }
-
-# output "alb_dns_name" {
-#   description = "Internal ALB DNS name"
-#   value       = module.alb.alb_dns_name
-# }
