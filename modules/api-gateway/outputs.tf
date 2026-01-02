f4a9e3a9ea726bcd6d414d6caeabc450d0d1630a
yaron kalatian <kalatianyaron@gmail.com>
2026-01-02 11:40:36 +0200

    dup

--- modules/api-gateway/outputs.tf
+++ modules/api-gateway/outputs.tf
@@ -6,7 +6,3 @@ output "vpc_link_security_group_id" {
   description = "Security group ID used by API Gateway VPC Link"
   value       = aws_security_group.vpc_link.id
 }
-
-output "api_endpoint" {
-  value = aws_apigatewayv2_api.abra.api_endpoint
-}
